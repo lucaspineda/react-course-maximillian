221d6dd218925a97875eb48781a7d2c3f74c50ce
lucas-theyes <lucas@theyes.com>
2021-11-08 23:50:45 -0300

    adding css style

--- react-complete-guide/01-starting-setup/src/components/ExpenseItem.js
+++ react-complete-guide/01-starting-setup/src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>
+  return (
+    <div className="expense-item">
+      <div>March 28th</div>
+      <div className="expense-item__description">
+        <div>Title</div>
+        <div className="expense-item__price">$56</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
