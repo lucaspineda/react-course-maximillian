b38358ca1106c5416cd78441490aa86a7c29fa78
lucas-theyes <lucas.pineda@hotmail.com>
2021-11-12 02:21:07 -0300

    create expenses component (assignment)

--- react-complete-guide/01-starting-setup/src/components/ExpenseItem.js
+++ react-complete-guide/01-starting-setup/src/components/ExpenseItem.js
@@ -1,6 +1,5 @@
+import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css';
-
-
 
 function ExpenseItem(props) {
   const month = props.date.toLocaleString('en-US', {month: 'long' });
@@ -9,11 +8,7 @@ function ExpenseItem(props) {
 
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
