124b25b8dd62055a8a955c3cc4cc3dc366a41ded
lucas-theyes <lucas.pineda@hotmail.com>
2021-11-30 01:31:42 -0300

    rendering list of data dynamically

--- react-complete-guide/01-starting-setup/src/components/Expenses/Expenses.js
+++ react-complete-guide/01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,41 +1,31 @@
 import ExpenseItem from "../Expenses/ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
-import { useState } from 'react'
+import { useState } from "react";
 
 import "../Expenses/Expenses.css";
 
 function Expenses(props) {
-  const [yearFiltered, setYearFiltered] = useState('2020');
+  const [yearFiltered, setYearFiltered] = useState("2020");
 
   const handleFilterChange = (year) => {
-    console.log('here in expenses.js', year)
-    setYearFiltered(year)
-  }
+    console.log("here in expenses.js", year);
+    setYearFiltered(year);
+  };
   return (
     <div>
-      <ExpensesFilter selected={yearFiltered} onChangeFilter={handleFilterChange} />
       <Card className="expenses">
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
+        <ExpensesFilter
+          selected={yearFiltered}
+          onChangeFilter={handleFilterChange}
         />
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        />
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        />
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        />
+        {props.expenses.map(expense => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
